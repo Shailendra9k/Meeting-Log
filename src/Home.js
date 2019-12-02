20c3ecc21760ffaf66dbd8d6b4f871746895dcb6
Shailendra Kushwaha <learning4succedding@gmail.com>
2019-12-02 03:00:04 +0200

    user configuration

--- src/Home.js
+++ src/Home.js
@@ -2,12 +2,24 @@ import React, { Component } from "react";
 
 class Home extends Component {
   render() {
+    const { user } = this.props;
+    const biggerLead = {
+      fontSize: 1.4 + "em",
+      fontWeight: 200
+    };
     return (
       <div className="container text-center">
         <div className="row justify-content-center">
           <div className="col-10 col-md-10 col-lg-8 col-xl-7">
-            <div className="display-4 text-primary mt-3 mb-2">Meeting Log</div>
-            <p className="lead">
+            <div
+              className="display-4 text-primary mt-3 mb-2"
+              style={{
+                fontSize: 2.8 + "em"
+              }}
+            >
+              Meeting Log
+            </div>
+            <p className="lead" style={biggerLead}>
               This simple app creates meetings, allows people to check in, and
               picks random users to award giveaways. It's a good example of a
               Single Page Application which includes connection to a database
@@ -16,15 +28,22 @@ class Home extends Component {
               <a href="https://firebase.google.com">Firebase</a>.
             </p>
 
-            <a href="/register" className="btn btn-outline-primary mr-2">
-              Register
-            </a>
-            <a href="/login" className="btn btn-outline-primary mr-2">
-              Log In
-            </a>
-            <a href="/meetings" className="btn btn-primary">
-              Meetings
-            </a>
+            {user == null && (
+              <span>
+                <a href="/register" className="btn btn-outline-primary mr-2">
+                  Register
+                </a>
+                <a href="/login" className="btn btn-outline-primary mr-2">
+                  Log In
+                </a>
+              </span>
+            )}
+
+            {user && (
+              <a href="/meetings" className="btn btn-primary">
+                Meetings
+              </a>
+            )}
           </div>{" "}
           {/* columns */}
         </div>

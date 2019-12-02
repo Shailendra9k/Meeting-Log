e308e7226e4d8694f158d508537a0b1994b52fd8
Shailendra Kushwaha <learning4succedding@gmail.com>
2019-12-02 03:39:41 +0200

    conditional components added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Home from "./Home";
 import Welcome from "./Welcome";
+import Navigation from "./components/Navigation";
 
 class App extends Component {
   constructor(props) {
@@ -14,6 +15,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <Navigation user={this.state.user} />
         {this.state.user && <Welcome user={this.state.user} />}
 
         <Home user={this.state.user} />
